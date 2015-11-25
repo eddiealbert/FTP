4cd9c6b6c2d8c14b3827ffb01c924135ee68ec17
User <User@gi231lab-16.lehman.edu>
2015-11-25 18:21:32 -0500

    Added table view controller for places

---
+++ FlickrTopPlaces/FTPTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTPTableViewController.h
+//  FlickrTopPlaces
+//
+//  Created by User on 11/25/15.
+//  Copyright (c) 2015 cmp464. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTPTableViewController : UITableViewController
+
+@end
